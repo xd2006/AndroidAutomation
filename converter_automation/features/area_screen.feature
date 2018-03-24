0c75899a281c5a5e9f5eecb1ff90881ef2a3d35b
Alexander Strateichuk <a.strateichuk@gmail.com>
2018-03-24 11:28:55 +0300

    First test

---
+++ converter_automation/features/area_screen.feature
@@ -0,0 +1,6 @@
+Feature: User is able to convert area units
+
+  Scenario: User is able to dismiss help
+    Given I land on help popup
+    When I click on Got it button
+    Then I land on Area screen
---
+++ converter_automation/features/step_definitions/area_screen_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I land on help popup$/) do
+  puts("User lands on Help popup")
+end
+
+When(/^I click on Got it button$/) do
+  puts("User clicks on Got it button")
+end
+
+Then(/^I land on Area screen$/) do
+  puts("User lands on Area screen")
+end
